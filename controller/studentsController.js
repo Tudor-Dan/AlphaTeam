fd21cba0c5a7eecf7b5619636a06db545f5b0a0b
iamalexstanciu <alexstanciu@outlook.it>
2022-11-29 12:04:25 +0200

    id1

--- controller/studentsController.js
+++ controller/studentsController.js
@@ -1,44 +1,54 @@
 const display = require("../view/display");
 const dataManager = require("../model/dataManager");
+const students = dataManager.readData("data.json");
 
 const getAllStudents = () => {
-    const students = dataManager.readData("data.json");
-    display.printData(students, "Students Table:");
+  display.printData(students, "Students Table:");
+};
+
+function numberStudents() {
+  display.printMessage(students.length);
 }
 
 const hasChosen = () => {
-    const userOption = display.getInput("Please enter a number: ");
-    if (userOption === "1") {
-        getAllStudents();
-    } else if (userOption === "2") {
-        display.printMessage("'Add student' not implemented yet.", true);
-    } else if (userOption === "3") {
-        display.printMessage("'Update student' not implemented yet.", true);
-    } else if (userOption === "4") {
-        display.printMessage("'Delete student' not implemented yet.", true);
-    } else if (userOption === "0") {
-        return false;
-    } else {
-        display.printMessage("There is no such option.", true);
-    }
-    return true;
-}
+  const userOption = display.getInput("Please enter a number: ");
+  if (userOption === "1") {
+    numberStudents();
+  } else if (userOption === "2") {
+    display.printMessage("'Add student' not implemented yet.", true);
+  } else if (userOption === "3") {
+    display.printMessage("'Update student' not implemented yet.", true);
+  } else if (userOption === "4") {
+    display.printMessage("'Delete student' not implemented yet.", true);
+  } else if (userOption === "0") {
+    return false;
+  } else {
+    display.printMessage("There is no such option.", true);
+  }
+  return true;
+};
 
 const handleSubmenu = () => {
-    const optionsArray = ["Exit submenu", "List students", "Add a new student", "Update student", "Delete student"];
-    display.printMenu("Student Classes Submenu", optionsArray);
-}
+  const optionsArray = [
+    "Exit submenu",
+    "List students",
+    "Add a new student",
+    "Update student",
+    "Delete student",
+  ];
+  display.printMenu("Student Classes Submenu", optionsArray);
+};
 
 const submenu = () => {
-    let isRunning = true;
-    while (isRunning) {
-        handleSubmenu();
-        try {
-            isRunning = hasChosen();
-        } catch (error) {
-            display.printMessage(error, true);
-        }
+  let isRunning = true;
+  while (isRunning) {
+    handleSubmenu();
+    try {
+      isRunning = hasChosen();
+    } catch (error) {
+      display.printMessage(error, true);
     }
-}
+  }
+};
 
-module.exports = {submenu};
+module.exports = { submenu };
